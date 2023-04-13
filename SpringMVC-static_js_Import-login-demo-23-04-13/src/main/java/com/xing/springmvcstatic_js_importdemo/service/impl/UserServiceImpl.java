933fc88b4ad350b39cf69d8a2e97dbca1fad5b1e
XGLins <2819194126@qq.com>
2023-04-13 22:56:57 +0800

    login验证

---
+++ SpringMVC-static_js_Import-login-demo-23-04-13/src/main/java/com/xing/springmvcstatic_js_importdemo/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.xing.springmvcstatic_js_importdemo.service.impl;
+
+import com.xing.springmvcstatic_js_importdemo.entity.User;
+import com.xing.springmvcstatic_js_importdemo.mapper.UserMapper;
+import com.xing.springmvcstatic_js_importdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author XGLins
+ * @date 2023/4/13 10:57
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    //重写通过用户姓名查询信息方法
+    @Override
+    public User getUserByNameAndPassword(String username, String password) {
+        return userMapper.getUserByNameAndPassword(username, password);
+    }
+
+    //查询所有用户信息
+    @Override
+    public List<User> getAllUser() {
+        return userMapper.getAllUser();
+    }
+}
