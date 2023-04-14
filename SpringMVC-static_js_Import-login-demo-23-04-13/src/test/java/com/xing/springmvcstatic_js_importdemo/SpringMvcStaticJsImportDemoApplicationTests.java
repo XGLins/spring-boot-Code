b3622386fad6de4f86b4c7fc6b60b3edfe04146e
XGLins <2819194126@qq.com>
2023-04-14 09:57:04 +0800

    login验证
    通过前端axios，res.data中存储的用户名/密码与前端输入的用户名/密码进行判断比较，成功则跳转，反之则提示。

---
+++ SpringMVC-static_js_Import-login-demo-23-04-13/src/test/java/com/xing/springmvcstatic_js_importdemo/SpringMvcStaticJsImportDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.xing.springmvcstatic_js_importdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcStaticJsImportDemoApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
